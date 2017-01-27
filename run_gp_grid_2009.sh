95ef673454632198753a7f470fe138e7f6ba5735
chenwang <chenwangnida@gmail.com>
2017-01-27 14:59:05 +1300

    update

--- run_gp_grid_2009.sh
+++ run_gp_grid_2009.sh
@@ -5,5 +5,5 @@ need sgegrid
 NUM_RUNS=40
 
 for i in {1..5}; do
-  qsub -t 1-$NUM_RUNS:1 webservice_gp.sh ~/workspace/swsc2009/Set0${i}MetaData 2009-semantic-unfoldgp-200-30${i};
+  qsub -t 1-$NUM_RUNS:1 webservice_gp.sh ~/workspace/swsc2009/Set0${i}MetaData 2009-semantic-unfoldgp-30-100${i};
 done
